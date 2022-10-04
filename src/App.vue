9ed022f47ed99f91a29a773f6aef864e9a25ccc4
Dongmin Kim <qlsnaos12@naver.com>
2022-10-04 17:54:36 +0900

    event

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,67 @@
 <template>
   <div id="app">
-    <DataBinding></DataBinding>
-    <p>-------------DataBinding 끝-------------</p>
-    <FoamBinding></FoamBinding>
-    <p>-------------FoamBinding 끝-------------</p>
-    <FoamBinding2></FoamBinding2>
-    <p>-------------FoamBinding2 끝-------------</p>
-    <FoamBinding3></FoamBinding3>
-    <p>-------------FoamBinding3 끝-------------</p>
-    <TestComponent></TestComponent>
+    <div class="binding_cont" v-if="bool === 1">
+      <DataBinding/>
+      <p>-------------DataBinding 끝-------------</p>
+      <FoamBinding/>
+      <p>-------------FoamBinding 끝-------------</p>
+      <FoamBinding2/>
+      <p>-------------FoamBinding2 끝-------------</p>
+      <FoamBinding3/>
+      <p>-------------FoamBinding3 끝-------------</p>
+      <TestComponent/>
+    </div>
+
+
+    <div class="event_cont" v-else-if="bool === 2" >
+      <VonComponent v-on:send="getEvent"/>
+      <ComputedComponent v-on:send="getEvent"/>
+      <watchComponent v-on:send="getEvent"/>
+    </div>
+
   </div>
+
 </template>
 
 <script>
-import DataBinding from "./views/DataBinding.vue";
-import FoamBinding from "./views/FoamBinding.vue";
-import FoamBinding2 from "./views/FoamBinding2.vue";
-import FoamBinding3 from "./views/FoamBinding3.vue";
+  // 데이터 바인딩
+import DataBinding from "./views/databinding/DataBinding.vue";
+import FoamBinding from "./views/databinding/FoamBinding.vue";
+import FoamBinding2 from "./views/databinding/FoamBinding2.vue";
+import FoamBinding3 from "./views/databinding/FoamBinding3.vue";
 import TestComponent from "./components/TestComponent.vue";
+import VonComponent from "./views/event/VonComponent.vue";
+import ComputedComponent from "./views/event/ComputedComponent.vue";
+import watchComponent from "./views/event/watchComponent.vue";
 
+// 이벤트
 export default {
   name: "App",
   components: {
+    // 데이터 바인딩
     DataBinding,
     FoamBinding,
     FoamBinding2,
     FoamBinding3,
     TestComponent,
+
+    // 이벤트
+    VonComponent,
+    ComputedComponent,
+    watchComponent,
+
+
   },
+  data() {
+    return {
+      bool: 2,      
+    }
+  },
+  methods: {
+    getEvent(){
+      alert('상위 컴포넌트로 이벤트전송 완료')
+    }
+  }
 };
 </script>
 
@@ -45,4 +79,5 @@ export default {
   color: #2c3e50;
   padding-top: 60px;
 }
+
 </style>
---
+++ src/views/event/watchComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    d
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
