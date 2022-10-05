899861dda793c707ceea7216eadbf4c7f0e65275
Dongmin Kim <qlsnaos12@naver.com>
2022-10-05 18:15:03 +0900

    add router

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <button @click="goToAbout">goToAbout</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  methods : {
+    goToAbout() {
+      // 이런식으로 라우터에 직접 접근하여 이동할수도 있다.
+      this.$router.push('/about')
+    }
+  }
+}
+</script>
