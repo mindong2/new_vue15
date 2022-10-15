1d63ebc656ba89860a15f24f487e2dbe199e5daf
mindong2 <qlsnaos12@naver.com>
2022-10-15 17:57:46 +0900

    propsdata

--- src/components/ChildView.vue
+++ src/components/ChildView.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="child">
-    <br/>
-    <br/>
-    <br/>
+    <br />
+    <br />
+    <br />
     {{ message }}
     <br />
     <br />
@@ -10,7 +10,7 @@
     <ul>
       <select multiple name="select" id="select" v-model="_selected">
         <option v-for="fruit in fruits" :key="fruit.id">
-        {{ fruit.id }} {{ fruit.name }}
+          {{ fruit.id }} {{ fruit.name }}
         </option>
       </select>
     </ul>
@@ -19,46 +19,47 @@
 </template>
 
 <script>
-    
 export default {
-    name: "ChildView",
-    props : {
-        message : {
-            type : String ,
-            default : ''
-        },
-        // array, object같은 reference속성들은 이렇게 선언해야한다. (그렇지않으면 모두 같은 객체를 공유하는 형태가 되어버리기 때문)
-        fruits: {
-          type:Array,
-          default: () => {
-            return []
-          }
-        },
-        selected: {
-          type:Array,
-          default: () => {
-            return []
-          }
-        }
-    },
-    // 자식에서의 props는 read-only이기 때문에 부모의 값을 변경해줄때는 함수를 호출하여 바꿔주면 된다.
-    computed: {
-      totalLength() {
-        return this.fruits.length;
+  name: "ChildView",
+  props: {
+    message: {
+      required: true, // 꼭필요하다
+      type: String,
+      default: "",
+      validator(value) {
+        //유효성 검사
+        return value === "Children propsdata"; //이 props가 맞는지 유효성 체크
       },
-      _selected: {
-        set(value){
-          this.$emit("change-select", value);
-        },
-        get(){
-          return this.selected;
-        }
-      }
-
-    }
-}
+    },
+    // array, object같은 reference속성들은 이렇게 선언해야한다. (그렇지않으면 모두 같은 객체를 공유하는 형태가 되어버리기 때문)
+    fruits: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+    selected: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+  },
+  // 자식에서의 props는 read-only이기 때문에 부모의 값을 변경해줄때는 함수를 호출하여 바꿔주면 된다.
+  computed: {
+    totalLength() {
+      return this.fruits.length;
+    },
+    _selected: {
+      set(value) {
+        this.$emit("change-select", value);
+      },
+      get() {
+        return this.selected;
+      },
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/ParentView.vue
+++ src/components/ParentView.vue
@@ -1,60 +1,56 @@
-  <!-- props -> :프롭스 속성 이름 = "상위 컴포넌트의 데이터 이름" -->
-  <template>
+<!-- props -> :프롭스 속성 이름 = "상위 컴포넌트의 데이터 이름" -->
+<template>
   <div id="parent">
     <h1>{{ propsdata }}</h1>
     <!-- <ChildView :message = "ㅎㅇㄹ"/> -->
     <ChildView
-     :message="str2"
-     :fruits="fruits"
-     :selected="selected"
-     @change-select="change"
-     ></ChildView>
+      :message="str2"
+      :fruits="fruits"
+      :selected="selected"
+      @change-select="change"
+    ></ChildView>
   </div>
 </template>
 
 <script>
-import ChildView from "./ChildView.vue"
-
+import ChildView from "./ChildView.vue";
 
 export default {
-    name : "ParentView",
-    data() {
-      return {
-        str2 : 'Children propsdata',
-        fruits : [
-          {id: 1, name: '사과', key: 'apple'},
-          {id: 2, name: '복숭아', key: 'peach'},
-          {id: 3, name: '오렌지', key: 'orange'},
-          {id: 4, name: '딸기', key: 'strawberry'}
-        ],
-        selected : [],
-      }
+  name: "ParentView",
+  data() {
+    return {
+      str2: "Children propsdata",
+      fruits: [
+        { id: 1, name: "사과", key: "apple" },
+        { id: 2, name: "복숭아", key: "peach" },
+        { id: 3, name: "오렌지", key: "orange" },
+        { id: 4, name: "딸기", key: "strawberry" },
+      ],
+      selected: [],
+    };
+  },
+  components: {
+    ChildView,
+  },
+  props: {
+    propsdata: {
+      type: String,
+      default: "",
     },
-    components : {
-      ChildView,
+  },
+  methods: {
+    change(value) {
+      this.selected = value;
     },
-    props: {
-        propsdata : {
-            type: String,
-            default : ''
-        }
+  },
+  watch: {
+    selected: {
+      handler(value) {
+        console.log(value);
+      },
     },
-    methods: {
-      change(value) {
-        this.selected = value;
-      }
-    },
-    watch : { 
-      selected : {
-        handler(value){
-          console.log(value)
-        }
-      }
-    }
-
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
