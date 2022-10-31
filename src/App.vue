b42621c4151ebdba6405baf96281a1f05e88ae77
Dongmin Kim <qlsnaos12@naver.com>
2022-10-31 13:42:54 +0900

    create branch 'vuex'

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <template>
   <div id="app">
-    <router-link to="/search">Search API</router-link>
-    <router-link to="/before">before_login API</router-link>
-    <br />
-    <br />
-    <router-view></router-view>
   </div>
 </template>
 
 <script>
   export default {
-    // Kakao 객체를 사용하기 위해 선언되지 않으면 정의하도록 설정
-    created() {
-    if(!window.Kakao.isInitialized()) {
-      window.Kakao.init(process.env.VUE_APP_KAKAO_JAVA_SCRIPT_KEY);
-      window.Kakao.isInitialized();
-    }
-  }
+
 } 
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import SearchView from "@/views/SearchView.vue";
-import BeforeLoginView from "@/views/BeforeLoginView.vue";
-import LoginView from "@/views/LoginView.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path : '/search',
-    name: "search",
-    component : SearchView
-  },
-  {
-    path : '/before',
-    name: "before",
-    component : BeforeLoginView
-  },
-  {
-    path : '/login',
-    name: "login",
-    component : LoginView
-  }
+  
 ];
 
 const router = new VueRouter({
